b38b23081923eec01118d27ff89c6d4f13fef467
NataliaSlabkiy <slabkiy@gmail.com>
2014-04-05 19:56:17 +0400

    JS check of form

--- web/dop/check.js
+++ web/dop/check.js
@@ -1,15 +1,10 @@
 /**
  * Created by Natalia on 01.04.2014.
  */
-function check(){
-    var r=document.forms["form"]["R"].value;
-    var y=document.forms["form"]["Y"].value;
-    var x=document.forms["form"]["X"].value;
 
-    if (r.length==0){
-        document.getElementById("errorR").innerHTML="*must be filled out";
-        alert("ddd");
-        return false;
+    function validRad(temp) {
+        var num = parseInt('gg');
+        if(isNaN(num)){alert('No');}
+        alert('No');
     }
 
-}
